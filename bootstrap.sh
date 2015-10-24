89910347e32d221ed5fa2133b5f0ef0aa06ba9e7
Pavel Stetina <stetina@shoptet.cz>
2015-10-24 23:31:14 +0200

    Setup PHP-FPM

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
+# shell enhancements
+if ! grep -q ll= /etc/bash.bashrc; then
+	echo -e '\n\nalias ll="ls -la"' | tee -a /etc/bash.bashrc
+fi
+
 # set timezone
-echo "Europe/Prague" | tee /etc/timezone
+echo "Europe/Prague" > /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
 
 # prepare locales
-cat /vagrant/etc/locale.gen | sudo tee /etc/locale.gen
+cat /vagrant/etc/locale.gen > /etc/locale.gen
 locale-gen
 
 # add dotdeb repository
-ln -s /etc/apt/sources.list.d/dotdeb.list /vagrant/etc/apt/sources.list.d/dotdeb.list
+cat /vagrant/etc/apt/sources.list.d/dotdeb.list > /etc/apt/sources.list.d/dotdeb.list
 curl https://www.dotdeb.org/dotdeb.gpg | apt-key add -
 
 # uninstall mess and install needed packages
 apt-get update
 apt-get purge -y apache2
 apt-get install -y nginx php7.0-fpm
+
+# setup webserver
+patch /etc/php/7.0/fpm/pool.d/www.conf < /vagrant/etc/php/7.0/fpm/pool.d/www.conf.patch --forward
