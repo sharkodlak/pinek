c8448b1ae8002948d7903427e3ff2d43b0748bf5
Pavel Stetina <stetina@shoptet.cz>
2015-12-08 10:50:55 +0100

    Allow to select which machines are to be installed

--- provision/virtualbox/guestadditions.sh
+++ provision/virtualbox/guestadditions.sh
@@ -12,55 +12,66 @@ function isCurrentVersion {
 
 function run {
 	read -r VM VM_STATUS PROVIDER REST <<< $1
-	for VM_TO_INSTALL in $2; do
-		if [[ $VM == $VM_TO_INSTALL ]]; then
-			if [[ $VM_STATUS == 'not' ]]; then
-				VM_STATUS="${VM_STATUS} ${PROVIDER}"
-				PROVIDER="$REST"
+	local INSTALL=
+	if [ -z "$2" ]; then
+		INSTALL=true
+	fi
+	if [ ! $INSTALL ]; then
+		for VM_TO_INSTALL in $2; do
+			if [[ $VM == $VM_TO_INSTALL ]]; then
+				INSTALL=true
+				break
 			fi
-			VBOXVM=$VM
-			echo "$VM: $VM_STATUS, $PROVIDER";
-			if [[ $VM_STATUS == 'saved' || $VM_STATUS == 'not created' ]]; then
-				echo "Bring up '$VM' because it is stopped or not created"
-				vagrant up $VM --no-provision
-			fi
-			if isCurrentVersion $VM $VERSION_TO_INSTALL; then
-				echo "Skip '$VM' - current installed version is equal or greater"
-			else
-				if [[ $VM_STATUS != 'poweroff' ]] ; then
-					echo "Halting '$VM'"
-					vagrant halt $VM
-				fi
-				echo "Attach ISO to '$VM' and bring machine up"
-				vboxmanage storageattach $VBOXVM --storagectl 'IDE Controller' --port 0 --device 1 --type dvddrive --medium /usr/share/virtualbox/VBoxGuestAdditions.iso
-				vagrant up $VM --no-provision
-				if isCurrentVersion $VM $VERSION_TO_INSTALL; then
-					echo "Skip '$VM' because current installed version is equal or greater"
-				else
-					echo "Build guest additions on '$VM'"
-					vagrant ssh $VM -c 'sudo apt-get install -y build-essential linux-headers-$(uname -r) && sudo mount /dev/cdrom /mnt && cd /mnt && sudo ./VBoxLinuxAdditions.run' < /dev/null
-				fi
-				echo "Halting machine '$VM' and detach ISO"
-				vagrant halt $VM
-				vboxmanage storageattach $VBOXVM --storagectl 'IDE Controller' --port 0 --device 1 --type dvddrive --medium none
-			fi
-			if [[ $VM_STATUS == 'running' ]]; then
-				vagrant up $VM
-			elif [[ $VM_STATUS == 'saved' ]]; then
-				vagrant up $VM
-				vagrant suspend $VM
-			fi
+		done
+	fi
+	if [ $INSTALL ]; then
+		if [[ $VM_STATUS == 'not' ]]; then
+			VM_STATUS="${VM_STATUS} ${PROVIDER}"
+			PROVIDER="$REST"
 		fi
-	done
+		VBOXVM=$VM
+		echo "$VM: $VM_STATUS, $PROVIDER";
+		if [[ $VM_STATUS == 'saved' || $VM_STATUS == 'not created' ]]; then
+			echo "Bring up '$VM' because it is stopped or not created"
+			vagrant up $VM --no-provision
+		fi
+		if isCurrentVersion $VM $VERSION_TO_INSTALL; then
+			echo "Skip '$VM' - current installed version is equal or greater"
+		else
+			if [[ $VM_STATUS != 'poweroff' ]] ; then
+				echo "Halting '$VM'"
+				vagrant halt $VM
+			fi
+			echo "Attach ISO to '$VM' and bring machine up"
+			vboxmanage storageattach $VBOXVM --storagectl 'IDE Controller' --port 0 --device 1 --type dvddrive --medium /usr/share/virtualbox/VBoxGuestAdditions.iso
+			vagrant up $VM --no-provision
+			if isCurrentVersion $VM $VERSION_TO_INSTALL; then
+				echo "Skip '$VM' because current installed version is equal or greater"
+			else
+				echo "Build guest additions on '$VM'"
+				vagrant ssh $VM -c 'sudo apt-get install -y build-essential linux-headers-$(uname -r) && sudo mount /dev/cdrom /mnt && cd /mnt && sudo ./VBoxLinuxAdditions.run' < /dev/null
+			fi
+			echo "Halting machine '$VM' and detach ISO"
+			vagrant halt $VM
+			vboxmanage storageattach $VBOXVM --storagectl 'IDE Controller' --port 0 --device 1 --type dvddrive --medium none
+		fi
+		if [[ $VM_STATUS == 'running' ]]; then
+			vagrant up $VM
+		elif [[ $VM_STATUS == 'saved' ]]; then
+			vagrant up $VM
+			vagrant suspend $VM
+		fi
+	fi
 }
 
 export -f run isCurrentVersion getVersion
 export VERSION_TO_INSTALL=$(dpkg -l | grep virtualbox-guest-additions | grep -oP '\d+\.\d+\.\d+')
 VM_STATUSES=$(vagrant status | grep -P '^\w+(?=\s{2,})')
+VMS_TO_INSTALL=$@
 echo 'Running guest additions install'
 printf "%s\n" "${VM_STATUSES[@]}"
-echo "Install on ${VMS_TO_INSTALL:=$@}"
-printf "%s\n" "${VM_STATUSES[@]}" | parallel --jobs 0 run {} "'${VMS_TO_INSTALL[@]}'"
-#printf "%s\n" "${VM_STATUSES[@]}" | while read -r VM_STATUS_LINE; do
-#	run "${VM_STATUS_LINE[@]}" "${VMS_TO_INSTALL[@]}"
-#done
+echo "Install on ${VMS_TO_INSTALL:-all machines}"
+#printf "%s\n" "${VM_STATUSES[@]}" | parallel --jobs 0 run {} "'${VMS_TO_INSTALL[@]}'"
+printf "%s\n" "${VM_STATUSES[@]}" | while read -r VM_STATUS_LINE; do
+	run "${VM_STATUS_LINE[@]}" "${VMS_TO_INSTALL[@]}"
+done
