a4b38637d66f949d9af420489bc2acc6e9df68c0
Pavel Stetina <stetina@shoptet.cz>
2016-02-17 17:10:00 +0100

    Do not use parallel in build (antcall spawn new processes with their own variables, so if something depends on same target, that target runs again)

--- tests/FailTest.php
+++ tests/FailTest.php
@@ -1,16 +1,7 @@
 <?php
 
 class FailTest extends PHPUnit_Framework_TestCase {
-	public function testPushAndPop() {
-		$stack = array();
-		$this->assertEquals(0, count($stack));
-
-		array_push($stack, 'foo');
-		$this->assertEquals('foo', $stack[count($stack)-1]);
-		$this->assertEquals(1, count($stack));
-
-		$this->assertEquals('foo', array_pop($stack));
-		$this->assertEquals(0, count($stack));
+	public function testFail() {
+		$this->assertEquals(0, 1);
 	}
 }
-?>
